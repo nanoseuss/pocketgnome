ee92ef1412e380b91ead5cb10bbc80eb17b14f9f
ootoaoo <ootoaoo@4a9c585c-cb2d-11dd-a3c7-fda619b15ae3>
2009-04-16 00:59:19 +0000

    Addeds more offsets - the object struct clearly isn't correct
    git-svn-id: http://pocketgnome.googlecode.com/svn/trunk/pocketgnome@34 4a9c585c-cb2d-11dd-a3c7-fda619b15ae3

--- Offsets.h
+++ Offsets.h
@@ -15,8 +15,8 @@
 #define PLAYER_LEVEL_CAP    80
 
 // not valid for PPC!
-// 3.0.9 valid
-#define PLAYER_NAME_STATIC          ((IS_X86) ? 0x1374028 : 0x0)       // 3.1.0
+// 3.1.0 valid
+#define PLAYER_NAME_STATIC          ((IS_X86) ? 0x1374028 : 0x0)        // 3.1.0
 											 // 0x14A6B28 : 0x0)        // 3.0.9
                                              // 0x14A9B48 : 0x0)        // 3.0.8
                                              // 0x14A27E8 : 0x0)        // 3.0.3
@@ -25,7 +25,7 @@
 
 
 
-#define SERVER_NAME_STATIC          ((IS_X86) ? 0x1374706 : 0x0)       // 3.1.0
+#define SERVER_NAME_STATIC          ((IS_X86) ? 0x1374706 : 0x0)        // 3.1.0
 											 // 0x14A6F86 : 0x0)        // 3.0.9
                                              // 0x14A9FA6 : 0x0)        // 3.0.8                                     
                                              // 0x14A2C46 : 0x0)        // 3.0.3
@@ -35,7 +35,7 @@
 
 
 
-#define ACCOUNT_NAME_STATIC         ((IS_X86) ? 0x1374180 : 0x0)       // 3.1.0
+#define ACCOUNT_NAME_STATIC         ((IS_X86) ? 0x1374180 : 0x0)        // 3.1.0
 											 // 0x14A6C80 : 0x0)        // 3.0.9
                                              // 0x14A9CA0 : 0x0)        // 3.0.8
                                              // 0x14A2940 : 0x0)        // 3.0.3
@@ -45,27 +45,17 @@
                                              // 0xEB0D46 : 0xEC8F9E  // 2.4.2
                                              // 0xEB0C80 : 0xEC8EE0  // 2.4.2
 
-// 3.0.9 valid	
+// 3.1.0 valid	
 #define PLAYER_GUID_STATIC          ((IS_X86) ? 0xAA3400 : 0x0) // 3.1.0
 											 // 0xB75420 : 0x0) // 3.0.9
                                              // 0xB78420 : 0x0) // 3.0.8
                                              // 0xB70980 : 0x0) // 3.0.2(0xB6C960)
                        
-// 3.0.9 valid	   0xAA6BA4
-#define OBJECT_LIST_PTR_STRUCT_ID   ((IS_X86) ? 0x0 : 0x0) // 3.1.0
+// 3.0.9 valid	   0xAA6BA4  0x13969698
+#define OBJECT_LIST_PTR_STRUCT_ID   ((IS_X86) ? 0x21185000 : 0x0) // 3.1.0
 											 // 0xB9BBE8 : 0x0) // 3.0.9
                                              // 0xB9EBE8 : 0x0) // 3.0.8
                                              // 0xB96C08 : 0x0) // 3.0.3 (0xB92BC8)
-
-// 2.4.2 valid
-// NO LONGER VALID AS OF 3.0.2
-#define PLAYER_STRUCT_PTR_STATIC    ((IS_X86) ? 0x9BA490 : 0x9E2460) // 2.4.3 (0x1DA0 : 0xD70);
-                                             // 0x9BC230 : 0x9E31D0  // 2.4.2
-                                             // 0x9B3210 : 0x9D81B0  // 2.4.1
-                                                        // 0x9D91B0 2.4.0 // 0x5B350 diffPPC
-                                 /* 0x96D3B0 2.3.3 Intel : 0x97DE60 2.3.3 PPC */
-                                                        /* 0x97DE50 2.3.2 PPC */
-                                                        /* 0x977E10 2.3.0 PPC */
 
 // 3.0.9 valid
 #define COMBO_POINTS_STATIC         ((IS_X86) ? 0xB76FD0 : 0x0) // 3.0.9
@@ -110,8 +100,9 @@
     #define TARGET_MOUSEOVER    0x30 /* GUID */
 // }
 
-// 3.0.9 valid
-#define KNOWN_SPELLS_STATIC             ((IS_X86) ? 0x1558240 : 0x0) // 3.0.9
+// 3.1.0 valid
+#define KNOWN_SPELLS_STATIC             ((IS_X86) ? 0x141FFE0 : 0x0) // 3.1.0
+												 // 0x1558240 : 0x0) // 3.0.9
                                                  // 0x155B260 : 0x0) // 3.0.8
                                                  // 0x1553E80 : 0x0) // 3.0.3
                                                  // 0x154FB40 : 0x0) // 3.0.2
@@ -132,9 +123,10 @@
                                                  // 0xF3E240 : 0xF56068  // 2.4.2
                                                  // 0xF347E0 : 0xF4A398  // 2.4.1
 
-// 3.0.9 valid
+// 3.1.0 valid
 // static main hotbar ( uint32[12], spell ID)
-#define HOTBAR_BASE_STATIC  ((IS_X86) ? 0x154A1E0 : 0x0) // 3.0.9
+#define HOTBAR_BASE_STATIC  ((IS_X86) ? 0x1412FA0 : 0x0) // 3.1.0
+									 // 0x154A1E0 : 0x0) // 3.0.9
                                      // 0x154D200 : 0x0) // 3.0.8
                                      // 0x1545E20 : 0x0) // 3.0.3
                                      // 0x1541AE0 : 0x0) // 3.0.2
@@ -160,24 +152,12 @@
                                                  // 0x11F0BAC : 0x0)        // 3.0.2
                                                  // 0xCAA0AC : 0xCC39EC)    // 2.4.2
 
-// both of these are busted in 3.0.2
-/* is the number of the last spell we tried to cast, nomatter if it went off or didn't */
-// shows up when you click a 'white' labeled spell
-// #define LAST_SPELL_ATTEMPTED_CAST_STATIC    ((IS_X86) ? 0xCE8DD0 : 0xD03760) // 2.4.3
-                                                     // 0xCE8650 : 0xD02138) // 2.4.2
-                                                     // 0xCDEFF0 : 0xCF6858) // 2.4.1
-                                                                // 0xCF6DA0 2.4.0
-                                                                // 0xC94980 2.3.3
-
-/* goes 0 if a spell fails (most of the time), spell id if it cast or started casting */
-// #define LAST_SPELL_ACTUALLY_CAST_STATIC     ((IS_X86) ? 0xCECEF0 : 0xD07874) // 2.4.3
-                                                     // 0xCE2D10 : 0xCFA56C) // 2.4.2
-                                                     // 0xCE2D10 : 0xCFA56C) // 2.4.1
-                                                                // 0xC9869C 2.3.3
 // 3.0.9 valid
 // as of 3.0.2, this value is 0 if a spell successfully cast
 // if the spell did not cast, it contains the ID of the most recently failed spell
 // only works for spells that play an error sound. not sure what makes this distinction.
+
+//
 #define LAST_SPELL_THAT_DIDNT_CAST_STATIC   ((IS_X86) ? 0x122BB98 : 0x0) // 3.0.9
                                                      // 0x122EBB8 : 0x0) // 3.0.8
                                                      // 0x1227C98 : 0x0) // 3.0.3
@@ -203,3 +183,34 @@
                                          // 0xA20F40 : 0xA37880) // 2.4.1 
                                          // 0xA37DD0 2.4.1 PPC
                                          // 0x9D79E0 2.3.3 PPC
+
+
+
+
+// **************************** NOT USED/DATED ******************************************* //
+
+// 2.4.2 valid
+// NO LONGER VALID AS OF 3.0.2
+#define PLAYER_STRUCT_PTR_STATIC    ((IS_X86) ? 0x9BA490 : 0x9E2460) // 2.4.3 (0x1DA0 : 0xD70);
+// 0x9BC230 : 0x9E31D0  // 2.4.2
+// 0x9B3210 : 0x9D81B0  // 2.4.1
+// 0x9D91B0 2.4.0 // 0x5B350 diffPPC
+/* 0x96D3B0 2.3.3 Intel : 0x97DE60 2.3.3 PPC */
+/* 0x97DE50 2.3.2 PPC */
+/* 0x977E10 2.3.0 PPC */
+
+// both of these are busted in 3.0.2
+/* is the number of the last spell we tried to cast, nomatter if it went off or didn't */
+// shows up when you click a 'white' labeled spell
+// #define LAST_SPELL_ATTEMPTED_CAST_STATIC    ((IS_X86) ? 0xCE8DD0 : 0xD03760) // 2.4.3
+// 0xCE8650 : 0xD02138) // 2.4.2
+// 0xCDEFF0 : 0xCF6858) // 2.4.1
+// 0xCF6DA0 2.4.0
+// 0xC94980 2.3.3
+
+/* goes 0 if a spell fails (most of the time), spell id if it cast or started casting */
+// #define LAST_SPELL_ACTUALLY_CAST_STATIC     ((IS_X86) ? 0xCECEF0 : 0xD07874) // 2.4.3
+// 0xCE2D10 : 0xCFA56C) // 2.4.2
+// 0xCE2D10 : 0xCFA56C) // 2.4.1
+// 0xC9869C 2.3.3
+
