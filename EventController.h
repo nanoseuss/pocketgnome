025f6090e9729096fd3052ff39ea9976fc10f1ad
ootoaoo <ootoaoo@4a9c585c-cb2d-11dd-a3c7-fda619b15ae3>
2009-12-03 17:41:30 +0000

    Forgot to commit
    git-svn-id: http://pocketgnome.googlecode.com/svn/trunk/pocketgnome@129 4a9c585c-cb2d-11dd-a3c7-fda619b15ae3

---
+++ EventController.h
@@ -0,0 +1,28 @@
+//
+//  EventController.h
+//  Pocket Gnome
+//
+//  Created by Josh on 11/23/09.
+//  Copyright 2009 Savory Software, LLC. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+#define EventZoneChanged					@"EventZoneChanged"
+#define EventBattlegroundStatusChange		@"EventBattlegroundStatusChange"
+
+@class Controller;
+@class PlayerDataController;
+
+@interface EventController : NSObject {
+	IBOutlet Controller *controller;
+	IBOutlet PlayerDataController *playerController;
+	
+	NSTimer *_uberQuickTimer;
+	NSTimer *_fiveSecondTimer;
+	
+	int _lastPlayerZone;
+	int _lastBGStatus;
+}
+
+@end
