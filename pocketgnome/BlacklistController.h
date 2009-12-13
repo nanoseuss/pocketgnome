1db3bd4cf014348fe4754246587b940c71c76175
ootoaoo <ootoaoo@4a9c585c-cb2d-11dd-a3c7-fda619b15ae3>
2009-12-13 19:24:58 +0000

    Stub, so I don't forget to commit
    git-svn-id: http://pocketgnome.googlecode.com/svn/trunk@138 4a9c585c-cb2d-11dd-a3c7-fda619b15ae3

---
+++ pocketgnome/BlacklistController.h
@@ -0,0 +1,16 @@
+//
+//  BlacklistController.h
+//  Pocket Gnome
+//
+//  Created by Josh on 12/13/09.
+//  Copyright 2009 __MyCompanyName__. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+
+@interface BlacklistController : NSObject {
+
+}
+
+@end
