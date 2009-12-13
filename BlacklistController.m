caa40aed22f31cb1cce487704c3380fef8a3b47d
ootoaoo <ootoaoo@4a9c585c-cb2d-11dd-a3c7-fda619b15ae3>
2009-12-13 19:24:58 +0000

    Stub, so I don't forget to commit
    git-svn-id: http://pocketgnome.googlecode.com/svn/trunk/pocketgnome@138 4a9c585c-cb2d-11dd-a3c7-fda619b15ae3

---
+++ BlacklistController.m
@@ -0,0 +1,14 @@
+//
+//  BlacklistController.m
+//  Pocket Gnome
+//
+//  Created by Josh on 12/13/09.
+//  Copyright 2009 __MyCompanyName__. All rights reserved.
+//
+
+#import "BlacklistController.h"
+
+
+@implementation BlacklistController
+
+@end
