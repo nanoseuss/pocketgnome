3cb747efa242967614e64268ed270eeba035a0f6
maodun@bigfoot.com <maodun@bigfoot.com@4a9c585c-cb2d-11dd-a3c7-fda619b15ae3>
2010-04-26 06:26:41 +0000

    BUG FIX: forgot to add MPItem.x
    git-svn-id: http://pocketgnome.googlecode.com/svn/branches/1.4-Pather@465 4a9c585c-cb2d-11dd-a3c7-fda619b15ae3

---
+++ MPAction.h
@@ -0,0 +1,24 @@
+//
+//  MPAction.h
+//  Pocket Gnome
+//
+//  Created by Coding Monkey on 4/24/10.
+//  Copyright 2010 Savory Software, LLC. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+
+/*!
+ * @class      MPAction
+ * @abstract   Represents an action in the game( spell, item, macro, etc..)
+ * @discussion 
+ * An action represents an action that can be performed using an actionID.
+ * Spells, Items, and Macros all seem to be performed using a unique ID.
+ *		
+ */
+@interface MPAction : NSObject {
+
+}
+
+@end
---
+++ MPAction.m
@@ -0,0 +1,14 @@
+//
+//  MPAction.m
+//  Pocket Gnome
+//
+//  Created by Coding Monkey on 4/24/10.
+//  Copyright 2010 Savory Software, LLC. All rights reserved.
+//
+
+#import "MPAction.h"
+
+
+@implementation MPAction
+
+@end
